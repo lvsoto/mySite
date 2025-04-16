8ddad21bb2b2307a734c94e9891d05e2ce1612dc
lvsoto <lvicentesoto2@gmail.com>
2025-04-16 15:02:28 -0400

    Updated contact pages with validation

---
+++ contactHandler.php
@@ -0,0 +1,43 @@
+<?php 
+
+//This is the validation section for data input
+$errors = '';
+$myemail = 'lvicentesoto2@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+//This is the section that will send the email if there are no errors
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contactThanks.html');
+} 
+else 
+{
+	//redirect to the 'error' page
+	header('Location: contactError.html');
+}
